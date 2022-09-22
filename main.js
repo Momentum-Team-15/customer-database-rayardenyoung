e940b513f7449e34e553d071c3b5539dade0e611
Ray Young <ray.arden.young@gmail.com>
2022-09-21 20:51:38 -0400

    evil cackle - i have a databasegit status aka data in squares

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //get the empty container of #person-container and store it in the variable personContainer
-const personContainer = document.querySelector("#person-container");
+const personContainer = document.querySelector("#profile-container");
 
 //loop through every customer of the customers Array
 for (let customer of customers) {
@@ -11,12 +11,13 @@ for (let customer of customers) {
     let customerDOB = document.createElement("p");
     let customerSince = document.createElement("p");
 //use the data from an object to populate these divs
-    customerName.innerText = customer.name;
-    //customerName's inner text will be the object's (customer) object (name)
+//customerName's (etc) inner text will be the object's (customer) nested object (name)
+    //call the nested objects by using . and separating objects nested within the same object
+    customerName.innerText = `${customer.name.first} ${customer.name.last}`;
     customerEmail.innerText = customer.email;
-    customerAddress.innerText = customer.location;
-    customerDOB.innerText = customer.dob;
-    customerSince.innerText = customer.registered;
+    customerAddress.innerText = `${customer.location.street.number} ${customer.location.street.name} ${customer.location.city} ${customer.location.state} ${customer.location.postcode}`;
+    customerDOB.innerText = customer.dob.date;
+    customerSince.innerText = customer.registered.date;
     
 //move all of this data into the customerDiv
     customerDiv.appendChild(customerName);
@@ -24,9 +25,10 @@ for (let customer of customers) {
     customerDiv.appendChild(customerAddress);
     customerDiv.appendChild(customerDOB);
     customerDiv.appendChild(customerSince);
-
+//move customerDiv into personContainer ??????????????????
     personContainer.appendChild(customerDiv);
-    personDiv.classList.add("customer")
+//add the class of customer to customerDiv for CSS stuff
+    customerDiv.classList.add("customer");
 
 
 
